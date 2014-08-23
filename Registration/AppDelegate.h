af931adb3fe4ced4e77f9db0eaa8abe3eca581d1
Dayanand Deshpande <Dayanand@Dayanands-MacBook-Air-2.local>
2014-08-24 10:04:27 +1200

    Initial Commit

---
+++ Registration/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Registration
+//
+//  Created by Dayanand Deshpande on 24/08/14.
+//  Copyright (c) 2014 Jay Deshpande. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Registration/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Registration
+//
+//  Created by Dayanand Deshpande on 24/08/14.
+//  Copyright (c) 2014 Jay Deshpande. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
